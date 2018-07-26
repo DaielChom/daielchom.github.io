7dfb5198317d3e73249e13f3bf8b863d7b7140a5
Daniel C Patiño <danielcarvajalpatino@gmail.com>
2018-07-25 19:23:31 -0500

    :pencil: Last changes of template

--- resume/src/App.js
+++ resume/src/App.js
@@ -49,9 +49,9 @@ class App extends Component {
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
-        
+
         <Testimonials data={this.state.resumeData.testimonials}/>
-        <Contact data={this.state.resumeData.main}/>
+        
         <Footer data={this.state.resumeData.main}/>
       </div>
     );
